852f698ac33d0ec561eaeb374329ce9097f85255
Chris Salvador <csalvador58@gmail.com>
2023-05-23 18:52:31 -0700

    updated with live coding work

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const router = Router();
 
+router.use('/landing', require('./landing'));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-const express = require("express");
+const express = require('express');
 
-const routes = require("./routes");
+const routes = require('./routes');
 
 const server = express();
 server.use(express.json());
+server.use(express.static('static'));
+
+const mustacheExpress = require('mustache-express');
+server.engine('mustache', mustacheExpress());
+server.set('view engine', 'mustache');
 
 server.use(routes);
 
