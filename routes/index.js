71f683aef88e1fd4ad9ca046b7ccc3499e4929d8
Chris Salvador <csalvador58@gmail.com>
2023-05-25 14:21:07 -0700

    Completed tests

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
 const { Router } = require('express');
 const router = Router();
 
+// router.use((req, res, next) => {
+//   console.log(`
+//   ${req.method} ${req.url}
+//   Headers: ${req.headers.authorization}
+//   Body: ${JSON.stringify(req.body)}
+//   at ${new Date()}`);
+//   next();
+// });
+
 router.use('/landing', require('./landing'));
 router.use('/weather', require('./weather'));
 
--- routes/weather.js
+++ routes/weather.js
@@ -1,8 +1,35 @@
 const { Router } = require('express');
 const router = Router();
 
+const weatherDAO = require('../daos/weather');
+const weather = require('../models/weather');
+
 router.get('/', async (req, res, next) => {
+  // console.log('Test - get /')
   res.render('weather');
+});
+
+router.get('/location', async (req, res, next) => {
+  // console.log('Test - get /location')
+  const locationName = req.query.name;
+  //   console.log('req.query.name');
+  //   console.log(req.query.name);
+
+  if (locationName) {
+    try {
+      const { name, temperature } = await weatherDAO.searchWeather(
+        locationName
+      );
+      res.status(name === 'Other' ? 404 : 200).render('weatherLocation', {
+        location: name,
+        temperature: temperature,
+      });
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  } else {
+    res.redirect('/weather');
+  }
 });
 
 module.exports = router;
